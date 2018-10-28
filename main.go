8753c7bcac4d9b837e2b2376d053e0f41401a008
Rus Hughes <russell.hughes@gmail.com>
2018-10-28 15:36:07 +0900

    s7 l77

--- main.go
+++ main.go
@@ -14,16 +14,24 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 	} else {
 		fmt.Println(link, "is up!")
+		c <- link
 	}
 }
